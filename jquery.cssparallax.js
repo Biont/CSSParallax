63c9defdc2682fbbb4da7256304ad745d0d66ec7
Moritz Meißelbach <arbelzapfgmail.com>
2013-04-20 16:48:17 +0200

    Initial commit

---
+++ jquery.cssparallax.js
@@ -0,0 +1,129 @@
+/*
+		  JQuery CSSparallax
+		  by Biont
+		  arbelzapf@gmail.com
+ 
+ 
+	MIT License
+	 
+	Copyright (c) 2013 Moritz Meiﬂelbach
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+	files (the "Software"), to deal in the Software without restriction, including without limitation the rights to 
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+	to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ */
+
+;(function( $ ){
+	// It might be useful to do this in the future. Disabled for now
+	
+   // // parallax public methods
+  // var methods = {
+    // setpos: function(name, callback) {
+
+    // },
+    // setfocus: function(name, callback) {
+
+    // }
+  // };
+
+  // $.parallax = function( method ) {
+
+    // if ( methods[method] ) {
+      // return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    // } else if ( typeof method === 'function' ||  typeof method === 'string'  || ! method ) {
+      // return methods.toggle.apply( this, arguments );
+    // } else {
+      // $.error( 'Method ' +  method + ' does not exist on jQuery.parallax' );
+    // }
+
+  // };
+
+  $.fn.parallax = function( options ) {
+
+    var settings = $.extend( {
+      interval       : 200,    // Accepts standard jQuery effects speeds (i.e. fast, normal or milliseconds)
+      speedX         : 400,   
+      speedY         : 100,    
+      falloff        :  0.4  ,
+      focus          :  0.9  ,
+      dof            :  null  ,
+      resize         :  false  ,
+    }, options);
+
+    return this.each(function(){
+	var $that = document.getElementById(this.id),
+		$this = $(this),
+		layers = $this.find('ul li'),
+		numlayers = layers.length,
+		layerids = [],
+		timer,
+		xpos,
+		ypos,
+		offset,
+		width,
+		height;
+
+	width = $that.offsetWidth;
+	height = $that.offsetHeight
+
+	
+	// Fetch all ids from layers and apply (optional) DoF
+	for (var index=0;index<numlayers; index++) {  
+	
+		layerids[index] = layers[index].id;
+	
+		if (settings.dof!=null){
+			var blur = 'blur(' + Math.abs(-settings.dof+index*1)*1.2 + 'px)';
+			layers[index].setAttribute("style", '-webkit-filter:'+ blur);
+		}
+	
+	}
+		// Mouseenter and Mouseleave functions wrapped in jQuery's hover
+		$this.hover(
+		  function () {
+		  
+			timer = setInterval(function(){
+				offset = [$that.style['left'],$that.style['top']];
+				//If resize is active, update dimensions (There might be a better way to do this)		
+				if(settings.resize){
+					width = $that.offsetWidth;
+					height = $that.offsetHeight
+				}
+				
+				//Get mouse position once (saves an event handler, but I'm not sure if this is the best way to do this)
+				//Calculate new position and tranlate the layers
+			   $(document).one("mousemove", function (e) {	   
+					xpos = (((e.pageX - offset[0])/width)-0.5)*settings.speedX;
+					ypos = (((e.pageY - offset[1])/height)-0.5)*settings.speedY; 
+					translate(xpos,ypos);
+				});
+
+				},settings.interval);
+		  },
+		  function () {
+		  //When mouse leaves, stop updating
+			clearInterval(timer);
+		  }
+		);
+				
+	function translate (newx,newy){
+			for (var index=0;index<numlayers; index++) {  
+				   var attrprefix = 'translate3d(',
+					   x = -newx*(-settings.focus+(index+1)*settings.falloff),
+					   y = -newy*((index+1)*settings.falloff);
+
+				   layers[index].style.webkitTransform = attrprefix + x + 'px,' + y + 'px,0)';
+				   layers[index].style.MozTransform = attrprefix + x + 'px,' + y + 'px,0)';
+				   layers[index].style.transform = attrprefix + x + 'px,' + y + 'px,0)';
+		};	
+	}
+	
+	
+    });
+  };
+
+})( jQuery );
