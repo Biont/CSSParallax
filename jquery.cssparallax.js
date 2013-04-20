971e56ab25665a37bcb08fe6e088df2192e35493
Moritz Meißelbach <arbelzapfgmail.com>
2013-04-20 16:54:06 +0200

    Initial commit

--- jquery.cssparallax.js
+++ jquery.cssparallax.js
@@ -1,22 +1,4 @@
-/*
-		  JQuery CSSparallax
-		  by Biont
-		  arbelzapf@gmail.com
- 
- 
-	MIT License
-	 
-	Copyright (c) 2013 Moritz Meiﬂelbach
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-	files (the "Software"), to deal in the Software without restriction, including without limitation the rights to 
-	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
-	to whom the Software is furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
- */
-
+/* CSSparallax | (c)2013 - Moritz Meiﬂelbach | MIT License */
 ;(function( $ ){
 	// It might be useful to do this in the future. Disabled for now
 	
